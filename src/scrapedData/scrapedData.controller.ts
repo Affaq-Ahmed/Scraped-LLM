3fe2a4465f5dbf8ba86fe04f7ef39673627a933e
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-18 12:13:38 +0500

    feat: add CRUDS for scrapedData

--- src/scrapedData/scrapedData.controller.ts
+++ src/scrapedData/scrapedData.controller.ts
@@ -1,8 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { ScrapedDataService } from './scrapedData.service';
+import { ScrapedDataDto } from 'src/dto/scrapedData.dto';
 
 @Controller('scraped-data')
 export class ScrapedDataController {
   constructor(private readonly scrapedDataService: ScrapedDataService) {}
   // Implement controller methods for CRUD operations
+
+  @Post()
+  @HttpCode(201)
+  async createScrapedData(@Body() scrapedData: ScrapedDataDto) {
+    return await this.scrapedDataService.createScrapedData(scrapedData);
+  }
+
+  @Get()
+  async getScrapedData() {
+    return await this.scrapedDataService.getScrapedData();
+  }
 }
