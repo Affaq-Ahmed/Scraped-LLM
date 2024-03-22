83432e1cd1485b3b541fadc6cf20b364a7eed38f
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-22 17:14:08 +0500

    Refactor: ScrapingCriteria model
    use 'name' instead of 'keywords', remove data range

--- src/scrapingCriteria/scrapingCriteria.model.ts
+++ src/scrapingCriteria/scrapingCriteria.model.ts
@@ -5,14 +5,8 @@ export type ScrapingCriteriaDocument = ScrapingCriteria & Document;
 
 @Schema({ timestamps: true })
 export class ScrapingCriteria {
-  @Prop({ type: [String] })
-  keywords: string[]; // array of keywords to search for
-
-  @Prop({ type: Date })
-  startDate: Date; // optional start date for scraping
-
-  @Prop({ type: Date })
-  endDate: Date; // optional end date for scraping
+  @Prop({ type: String })
+  name: string; // array of keywords to search for
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Source' })
   sourceId?: string; // optional reference to a specific source (if scraping from specific source)
